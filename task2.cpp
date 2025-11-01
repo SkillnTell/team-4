b662bbeddfd80401530807c7af887806ef9c68a3
yasmineziani-max <yasmine.ziani@ensia.edu.dz>
2025-10-31 17:21:31 -0700

    adding task2.cpp to the work

---
+++ task2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int getSum(int a, int b) {
+   
+    return a + b;
+}
+
+int main(){
+int a, b;
+cout<<" the sum of "<<a<<" and "<<b<< " is :"<<getSum(a,b)<<'\n';
+
+
+}
